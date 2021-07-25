6fffeb55998300a826ee4898a721d33ce9058a7c
Alexandria Guo <44476083+alexxg98@users.noreply.github.com>
2021-07-25 18:55:04 -0400

    Uncomment list items

--- timeline.js
+++ timeline.js
@@ -1,20 +1,20 @@
 const women = [
   "1842-3 | Ada Lovelace",
   "Known as the first computer programmer. Lovelace was the first to publish an algorithm intended to be carried out by a machine.",
-  // "1952 | Grace Hopper",
-  // "Developed the first compiler, A-o.",
-  // "1965 | Mary Allen Wilkes",
-  // "First developer of an operating system, LAP. Wilkes was the first person to use a computer in their personal home.",
-  // "1965 | Sister May Kenneth Keller",
-  // "First American women to earn a Ph.D in computer science.",
-  // "2006 | Melanie Rieback",
-  // "Programmed the first virus to infect RFID devices.",
-  // "2006 | Janet Emerson Bashen",
-  // "First African American women to hold a patent for a software invention.",
-  // "2009 | Luxia Zhang",
-  // "Awarded the IEEE Internet Award for her contributions toward developing the internet's architecture.",
-  // "2014 | Coraline Ada Ehmike",
-  // "Drafts the first code of conduct for open source projects, the Contributor Covenant."
+  "1952 | Grace Hopper",
+  "Developed the first compiler, A-o.",
+  "1965 | Mary Allen Wilkes",
+  "First developer of an operating system, LAP. Wilkes was the first person to use a computer in their personal home.",
+  "1965 | Sister May Kenneth Keller",
+  "First American women to earn a Ph.D in computer science.",
+  "2006 | Melanie Rieback",
+  "Programmed the first virus to infect RFID devices.",
+  "2006 | Janet Emerson Bashen",
+  "First African American women to hold a patent for a software invention.",
+  "2009 | Luxia Zhang",
+  "Awarded the IEEE Internet Award for her contributions toward developing the internet's architecture.",
+  "2014 | Coraline Ada Ehmike",
+  "Drafts the first code of conduct for open source projects, the Contributor Covenant."
 ];
 var speed = 50;
 var i = 0;
